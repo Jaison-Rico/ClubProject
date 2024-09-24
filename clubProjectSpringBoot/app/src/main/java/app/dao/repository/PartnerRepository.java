28713fbbd2a20f966dfd3dd7550a12dce63b9b64
owenunda <oweenunda.dev@gmail.com>
2024-09-24 00:56:57 -0500

    xd

--- clubProjectSpringBoot/app/src/main/java/app/dao/repository/PartnerRepository.java
+++ clubProjectSpringBoot/app/src/main/java/app/dao/repository/PartnerRepository.java
@@ -1,6 +1,12 @@
 
 package app.dao.repository;
 
+import app.model.Partner;
+
 public interface PartnerRepository {
+
+    public void save(Partner partner);
+
+    public void Delete(Partner partner);
     
 }
