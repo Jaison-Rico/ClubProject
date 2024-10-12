73bdb970b24931581e417c8f7bc10ab7c4f2c183
owenunda <oweenunda.dev@gmail.com>
2024-10-12 00:10:20 -0500

    fix invoiceValidator

--- clubProjectSpringBoot/app/src/main/java/app/controller/validator/InvoiceValidator.java
+++ clubProjectSpringBoot/app/src/main/java/app/controller/validator/InvoiceValidator.java
@@ -1,7 +1,15 @@
 
 package app.controller.validator;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
 public class InvoiceValidator extends CommonsValidator{
     public double validAmount(String amount) throws Exception{
         return super.isValidDouble("La valor de la factura", amount);
